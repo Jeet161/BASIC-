005192fdd640cd7a9ff93f09d71aaf5c858f3f3e
Jeet Dey <connect.jeetdey@gmail.com>
2026-02-22 00:05:53 +0530

    Add files via upload

---
+++ num range.py
@@ -0,0 +1,8 @@
+num=int(input("enter a number"))
+if num>0:
+    if num<=50:
+       print("number is between 1 and 50")
+    else:
+        print("number is greater than 50")
+else:
+    print("number is not possible")
