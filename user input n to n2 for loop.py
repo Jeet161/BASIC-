d02bbaa78e3a6ce4f51beaaf3c875e879d7b3271
Jeet Dey <connect.jeetdey@gmail.com>
2026-03-05 00:35:08 +0530

    Add files via upload

---
+++ user input n to n2 for loop.py
@@ -0,0 +1,9 @@
+n=int (input("enter first number:"))
+for i in range(0,n):
+  print(i,end="")
+  
+n=int (input("enter first number:"))
+n2=int (input("enter second number:"))
+for i in range(n2,n):
+    print (i,end="")
+    
